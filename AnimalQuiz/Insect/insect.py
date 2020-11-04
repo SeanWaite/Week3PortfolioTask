1f9a5f71334081c9ef8d9af5f01bf84c0eb067a7
SeanWaite <bh83dr@student.sunderland.ac.uk>
2020-11-04 15:48:48 +0000

    Commiting all initial files

---
+++ AnimalQuiz/Insect/insect.py
@@ -0,0 +1,35 @@
+import Animal.animal as a
+
+
+# Insect class is a child of Animal and adds a feature of insects
+class Insect(a.Animal):
+    def __init__(self, species, eyes, legs, wings):
+        super().__init__(species, eyes, legs)
+        self.wings = wings
+
+    # Below method gives the correct answer
+    def print_insect(self):
+        print("The insect is a " + self.species + ".", "It has " + self.eyes +
+              " eyes, " + self.legs + " legs and " + self.wings + " wings.\n")
+
+    # Bonus question to be asked
+    def insect_bonus_question(self):
+        self.wings = input("How many wings does a " + self.species + " have?\n")
+
+    # Return answer given
+    def insect_bonus_answer(self):
+        return self.wings
+
+    # Set back to correct answer
+    def insect_right_answer(self, wings):
+        self.wings = wings
+
+    def __str__(self):
+        return "\nPassed in values\nInsect is: " + self.species + \
+               "\nEyes: " + self.eyes + \
+               "\nLegs: " + self.legs + \
+               "\nWings: " + self.wings
+
+    def __repr__(self):
+        return 'Insect(species=%s, eyes=%s, legs=%s, wings=%s)' % \
+                      (self.species, self.eyes, self.legs, self.wings)
